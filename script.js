9af77aebbe57d4cb2daf6d1865adf07a92a17ed3
jamDEV3 <tyler_j83@outlook.com>
2023-08-21 13:58:39 +0100

    Fade JS animation

--- script.js
+++ script.js
@@ -5,15 +5,17 @@ let x = 0;
 // Name to be typed
 let name = 'Jamie Tyler';
 let desc = 'Web Developer || Game Developer';
+// let typename = document.getElementById("typename");
+// let typedesc = document.getElementById("typedesc");
 
 // Speed in ms
-let typeSpeed = 50;
+let speed = 40;
 
 function typename() {
     if (i < name.length) {
         document.getElementById("typename").innerHTML += name.charAt(i);
         i++;
-        setTimeout(typename, typeSpeed);
+        setTimeout(typename, speed);
     }
 }
 
@@ -21,18 +23,34 @@ function typedesc() {
     if (x < desc.length) {
         document.getElementById("typedesc").innerHTML += desc.charAt(x);
         x++;
-        setTimeout(typedesc, typeSpeed);
+        setTimeout(typedesc, speed);
     }
 }
 
 // --- FADE ---
 
+let opacity =0;
+let body = document.getElementById("body");
+body.style.opacity = 0;
 
+function show()
+{
+    opacity = Number(window.getComputedStyle(body).getPropertyValue("opacity"));
+    if (opacity < 1){
+        opacity=opacity+0.05;
+        body.style.opacity=opacity;
+        setTimeout(show, speed);
+    }
+
+}
 
 
 
 // --- EXECUTION ---
+
 setTimeout(() => {typename();}, 500)
 
 
-setTimeout(() => {typedesc();}, 1500);
+setTimeout(() => {typedesc();}, 1100);
+
+setInterval(show, 1500);
